41b748e72e76caea92c849312d1b13736c50003a
KatianaAlmeida <Katianaalmeida48@gmail.com>
2026-01-02 06:46:52 +0100

    make the cart quantity interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -113,6 +113,13 @@ document.querySelectorAll('.js-add-to-cart-button').forEach((buttonElement, inde
         quantity: 1
       })
     }
-    console.log(cart)
+
+    // calculate cart quantity
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
   })
 });
