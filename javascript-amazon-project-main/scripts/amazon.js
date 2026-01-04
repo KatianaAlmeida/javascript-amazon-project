70345093becca80e6d7799379f7396cd76599271
KatianaAlmeida <Katianaalmeida48@gmail.com>
2026-01-04 21:14:03 +0100

    added the file money.js

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -36,6 +36,7 @@ const products = [
 
 import {cart, addToCart} from '../data/cart.js'
 import { products } from '../data/products.js';
+import { formatCurrency } from '../utils/money.js';
 
 let productsHTML = '';
 
